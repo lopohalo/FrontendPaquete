c4121a6371a55ed5942b33b1c543ce605cab275b
SrJuan <79216367+lopohalo@users.noreply.github.com>
2022-02-14 10:24:57 -0500

    2commit
    en esta seccion del proyecto intentare darle estilos y logica a los botones

--- js/formulario.js
+++ js/formulario.js
@@ -2,19 +2,18 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-	apellido: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,// Letras y espacios, pueden llevar acentos.
 	ciudad: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	celular: /^\d{10}$/ 
 }
 
 const campos = {
 	apellido: false,
 	nombre: false,
 	correo: false,
-	celular: false
+	celular: false,
+	direccion: false
 }
 
 const validarFormulario = (e) => {
@@ -31,22 +30,19 @@ const validarFormulario = (e) => {
 		case "celular":
 			validarCampo(expresiones.celular, e.target, 'celular');
 		break;
+		case "direccion":
+			validarCampo(expresiones.direccion, e.target, 'direccion');
 	}
 }
 
 const validarCampo = (expresion, input, campo) => {
 	if(expresion.test(input.value)){
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
 		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
 		campos[campo] = true;
 	} else {
 		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
 		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
 		campos[campo] = false;
 	}
@@ -62,8 +58,7 @@ inputs.forEach((input) => {
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	const terminos = document.getElementById('terminos');
-	if(campos.apellido && campos.nombre && campos.ciudad && campos.correo && campos.celular && terminos.checked ){
+	if(campos.apellido && campos.nombre && campos.ciudad && campos.correo && campos.celular && campos.direccion){
 		formulario.reset();
 
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
@@ -71,10 +66,7 @@ formulario.addEventListener('submit', (e) => {
 			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
 		}, 5000);
 
-		document.querySelectorAll('.formulario__grudfpo-correcto').forEach((icono) => {
-			icono.classList.remove('formulario__grupo-correcto');
-		});
 	} else {
-		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	
 	}
 });

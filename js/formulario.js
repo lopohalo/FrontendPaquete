8c4a3c13af6c681f42c5be9d3b253b35224d2094
SrJuan <79216367+lopohalo@users.noreply.github.com>
2022-02-14 20:28:03 -0500

    commit5
    arreglo de mensaje de error colocandole un set time out de aproximadamente 2 seg

--- js/formulario.js
+++ js/formulario.js
@@ -73,7 +73,9 @@ formulario.addEventListener('submit', (e) => {
 		}, 5000);
 
 	} else {	
-  	document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-		
+	  document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	  setTimeout(() => {
+		  document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+	  }, 1000);
 	}
 });
